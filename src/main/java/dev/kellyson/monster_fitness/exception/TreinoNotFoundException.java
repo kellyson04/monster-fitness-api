1a62fa1979d5ba60f9d75cda66cef03fbdd02c03
Kell <gilsonvigilante@outlook.com>
2026-07-04 17:57:11 -0300

    feat: adiciona exceção para treino não encontrado

---
+++ src/main/java/dev/kellyson/monster_fitness/exception/TreinoNotFoundException.java
@@ -0,0 +1,8 @@
+package dev.kellyson.monster_fitness.exception;
+
+public class TreinoNotFoundException extends RuntimeException {
+
+    public TreinoNotFoundException(String message) {
+        super(message);
+    }
+}
