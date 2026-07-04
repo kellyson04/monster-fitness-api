322ff286cc07792652630d15238c3728ea7050a5
Kell <gilsonvigilante@outlook.com>
2026-07-04 17:57:10 -0300

    feat: adiciona dto de criação de treino

---
+++ src/main/java/dev/kellyson/monster_fitness/treino/dto/CriarTreinoRequest.java
@@ -0,0 +1,15 @@
+package dev.kellyson.monster_fitness.treino.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record CriarTreinoRequest(
+        @NotBlank(message = "O nome é obrigatório")
+        @Size(max = 100, message = "O nome deve ter no máximo 100 caracteres")
+        String nome,
+
+        @NotBlank(message = "A descrição é obrigatória")
+        @Size(max = 10000, message = "A descrição deve ter no máximo 10000 caracteres")
+        String descricao
+) {
+}
