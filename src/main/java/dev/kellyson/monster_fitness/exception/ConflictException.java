58857eff90ae9f3aa753fd95312a89aedd59bdad
Kell <gilsonvigilante@outlook.com>
2026-07-04 17:19:16 -0300

    feat: adiciona exceção para conflitos

---
+++ src/main/java/dev/kellyson/monster_fitness/exception/ConflictException.java
@@ -0,0 +1,8 @@
+package dev.kellyson.monster_fitness.exception;
+
+public class ConflictException extends RuntimeException {
+
+    public ConflictException(String message) {
+        super(message);
+    }
+}
