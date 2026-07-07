21f14b1ca564d9561c73b93c0eccf7d983a1f210
Kell <gilsonvigilante@outlook.com>
2026-07-07 18:14:09 -0300

    feat: adiciona timestamp na resposta de erro

--- src/main/java/dev/kellyson/monster_fitness/exception/ErrorResponse.java
+++ src/main/java/dev/kellyson/monster_fitness/exception/ErrorResponse.java
@@ -2,10 +2,18 @@ package dev.kellyson.monster_fitness.exception;
 
 import lombok.Builder;
 
+import java.time.Instant;
+
 @Builder
 public record ErrorResponse(
+        Instant timestamp,
         int status,
         String error,
         String message
 ) {
+    public ErrorResponse {
+        if (timestamp == null) {
+            timestamp = Instant.now();
+        }
+    }
 }
